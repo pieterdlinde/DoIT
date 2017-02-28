779835849c6ad89016554c2252c96f11d76001f9
Pieter Linde <pieterlinde@Pieters-Mac.local>
2017-02-28 13:31:45 +0200

    Test

--- DoIT/DoIT/ViewController.swift
+++ DoIT/DoIT/ViewController.swift
@@ -4,20 +4,37 @@
 //
 //  Created by Pieter Linde on 2017/02/28.
 //  Copyright © 2017 PieterLinde. All rights reserved.
-// 
+//
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView1: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+    
+    
 
 
 }
